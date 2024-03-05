ae9e1a8b5631238ce459bf9d154b0054a64611bc
wdelcant <wilsondelcanto.redes@gmail.com>
2024-03-04 23:28:30 -0300

    Update project configuration and styles

--- templates/react-eslint-tw/project/src/App.jsx
+++ templates/react-eslint-tw/project/src/App.jsx
@@ -1,16 +1,18 @@
 function App() {
-
-const name = 'React Eslint Tailwind'
+  const name = 'React Eslint Tailwind';
 
   return (
     <main className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
-      <header className="text-xl font-bold leading-[4rem] text-center">{name}</header>
-      <section className="py-8">👋</section>
+      <header className="text-xl font-bold leading-[4rem] text-center">
+        {name}
+      </header>
+      <h2 className="text-2xl font-bold text-center">Hello World 👋</h2>
+      <section className="py-8"></section>
       <footer className="text-center leading-[4rem] opacity-70">
         © {new Date().getFullYear()} {name}
       </footer>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- templates/react-eslint-tw/project/vite.config.js
+++ templates/react-eslint-tw/project/vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
   plugins: [react()],
-  server: {
-    open: true,
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
 })
